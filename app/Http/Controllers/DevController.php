c3f56246420f0b3e3308f73fdd27cb68507269fe
AbdulazizMualla <amgad74@hotmail.com>
2023-05-13 22:43:35 +0300

    change coloer 20

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -8,7 +8,7 @@ class DevController extends Controller
 {
     public function index(): \Illuminate\Http\JsonResponse
     {
-        $output = shell_exec('git pull');
+        $output = shell_exec('sudo git pull');
         return response()->json($output);
     }
 }
