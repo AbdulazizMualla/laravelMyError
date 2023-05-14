c6a44f988a6c0c860a47aa9bad35e65600dfd077
AbdulazizMualla <amgad74@hotmail.com>
2023-05-14 19:53:26 +0300

    chdir

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -10,10 +10,7 @@ class DevController extends Controller
     public function index(): \Illuminate\Http\JsonResponse
     {
         chdir('/Applications/XAMPP/xamppfiles/htdocs/laravelMyError');
-        shell_exec('git add . 2>&1');
-        shell_exec('git commit -m "commit from postman" 2>&1');
-        $output = shell_exec('git push 2>&1');
-       // $output = shell_exec('git pull 2>&1');
+        $output = shell_exec('git pull 2>&1');
         //exec('git pull');
         return response()->json($output);
     }
