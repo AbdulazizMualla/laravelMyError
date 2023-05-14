c7e5d906a8186ae134eb8ad0e6ac00fdea268b26
AbdulazizMualla <amgad74@hotmail.com>
2023-05-14 19:52:19 +0300

    commit from postman

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class DevController extends Controller
 {
     public function index(): \Illuminate\Http\JsonResponse
     {
-        $output = shell_exec('git pull');
+        chdir('/Applications/XAMPP/xamppfiles/htdocs/laravelMyError');
+        shell_exec('git add . 2>&1');
+        shell_exec('git commit -m "commit from postman" 2>&1');
+        $output = shell_exec('git push 2>&1');
+       // $output = shell_exec('git pull 2>&1');
         //exec('git pull');
         return response()->json($output);
     }
