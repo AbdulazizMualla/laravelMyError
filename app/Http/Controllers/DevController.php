a11960f234367ef7074f27539e7a20a04a936730
AbdulazizMualla <amgad74@hotmail.com>
2023-05-14 19:33:24 +0300

    git webhook test

--- app/Http/Controllers/DevController.php
+++ app/Http/Controllers/DevController.php
@@ -9,7 +9,7 @@ class DevController extends Controller
     public function index(): \Illuminate\Http\JsonResponse
     {
         $output = shell_exec('git pull');
-        exec('git pull');
+        //exec('git pull');
         return response()->json($output);
     }
 }
